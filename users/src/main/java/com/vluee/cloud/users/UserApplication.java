2bd23ea750e8fbf4ee3ae96349f802ef29b00dc4
Sean Ye <234aini@gmail.com>
2021-01-11 14:41:26 +0800

    完善用户服务

--- users/src/main/java/com/vluee/cloud/users/UserApplication.java
+++ users/src/main/java/com/vluee/cloud/users/UserApplication.java
@@ -6,11 +6,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Import;
 
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
+
+@ComponentScans({
+        @ComponentScan("cn.hutool.extra.spring"),
+        @ComponentScan("com.vluee.cloud.users")
+})
 @Import({IdConfig.class, BaseAuditConfig.class})
 public class UserApplication {
 
--- users/src/main/java/com/vluee/cloud/users/interfaces/command/rest/UserCommandController.java
+++ users/src/main/java/com/vluee/cloud/users/interfaces/command/rest/UserCommandController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/users")
 public class UserCommandController {
 
     @Autowired
