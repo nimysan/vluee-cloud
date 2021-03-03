f5a208bda36db6524d7009e7f8974cb1f40fe7e7
Sean Ye <234aini@gmail.com>
2021-03-03 16:14:24 +0800

    改进domaineventpublisher能力

--- commons/src/main/java/com/vluee/cloud/commons/ddd/support/infrastructure/events/SimpleDomainEventPublisher.java
+++ commons/src/main/java/com/vluee/cloud/commons/ddd/support/infrastructure/events/SimpleDomainEventPublisher.java
@@ -11,8 +11,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.web.context.ContextLoader;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -27,11 +32,24 @@ public class SimpleDomainEventPublisher implements DomainEventPublisher, Applica
         this.domainEventRepository = domainEventRepository;
         //新启动一个线程补偿
         log.info("Trigger events remedy action at Thread {}", Thread.currentThread().getName());
+        //TODO 如何实现补偿，因为事务问题，暂时没有很好的办法
         ThreadUtil.execAsync(new Runnable() {
             @Override
             public void run() {
                 //事务在多线程下不工作 详细： https://blog.51cto.com/13175304/1952201 和代码：TransactionSynchronizationManager
-                //SimpleDomainEventPublisher.this.eventualConsistencyRemedyAction();
+
+                DefaultTransactionDefinition def = new DefaultTransactionDefinition();
+                def.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+                PlatformTransactionManager txManager = ContextLoader.getCurrentWebApplicationContext().getBean(PlatformTransactionManager.class);
+                TransactionStatus status = txManager.getTransaction(def);
+
+                try {
+                    SimpleDomainEventPublisher.this.eventualConsistencyRemedyAction();
+                    txManager.commit(status); // 提交事务
+                } catch (Exception e) {
+                    log.info("异常信息：" + e.toString());
+                    txManager.rollback(status); // 回滚事务
+                }
             }
         }, true);
     }
@@ -51,17 +69,27 @@ public class SimpleDomainEventPublisher implements DomainEventPublisher, Applica
         applicationContext = this.applicationContext;
     }
 
+    /**
+     * 事件处理必须保持与外部事务一起工作
+     *
+     * @param event
+     */
     @Override
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @Transactional(propagation = Propagation.MANDATORY)
     public void publish(Serializable event) {
         SimpleDomainEvent simpleDomainEvent = new SimpleDomainEvent(AggregateId.generate(), DateUtil.date(), false, event);
         domainEventRepository.save(simpleDomainEvent);
-        log.info("Event is saved ... {}", simpleDomainEvent);
-        internalDoPublish(simpleDomainEvent);
+        try {
+            //任何实际分发的异常，都不回导致当前事务回滚，确保 "事件保存是成功的"
+            internalDoPublish(simpleDomainEvent);
+        } catch (Throwable e) {
+            //
+        }
+
     }
 
 
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @Transactional
     public void internalDoPublish(SimpleDomainEvent eventEntity) {
         final Serializable sourceEvent = eventEntity.getSourceEvent();
         for (EventHandler handler : new ArrayList<EventHandler>(eventHandlers)) {
